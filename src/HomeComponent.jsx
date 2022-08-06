08005a68ac2249fd52ae5ace2ec68014b44738bc
Sean Narron <seannarron9@gmail.com>
2022-08-06 17:22:53 +0900

    Made a table mapping state items (not working yet)

--- src/HomeComponent.jsx
+++ src/HomeComponent.jsx
@@ -9,7 +9,8 @@ class HomeComponent extends React.Component {
         super(); 
         this.state = {
             count: 0,
-            url: "https://62eddc42a785760e677041dd.mockapi.io/crud",
+            // url: "https://62eddc42a785760e677041dd.mockapi.io/crud"
+            url: "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e7946f7765931c840a35548ad3fa50c4",
             data: {}
         }
     }
@@ -20,10 +21,6 @@ class HomeComponent extends React.Component {
                 data: response.data
             })
         })
-    }
-
-    createUser = () => {
-
     }
 
     componentDidMount = () => {
@@ -41,16 +38,44 @@ class HomeComponent extends React.Component {
                 <div className="content">
                     <div className="table">
                         <table>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                        
-                            <tr>
-                                <td>abc</td>
-                                <td>def</td>
-                            </tr>
-                            
+                            <thead>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <td>{this.state.data.name}</td>
+                                    {/* {this.state.data.map(
+                                        data => (
+                                            <tr key={data.id}>
+                                                <td>{data.name}</td>
+                                                <td>{data.weather.description}</td>
+                                            </tr>
+                                                
+                                        )
+                                    )
+                                } */}
+                                    {/* <td>{this.state.data.map(data => {data.weather.description})}</td> */}
+                                </tr>
+                                
+                                {/* {
+                                this.state.data.map(
+                                        data => (
+                                            <tr key={data.id}>
+                                                <td>{data.name}</td>
+                                                <td>{data.weather.description}</td>
+                                            </tr>
+                                                
+                                        )
+                                    )
+                                } */}
+                                
+                            </tbody>
                         </table>
-                    </div>
+                    </div> 
+                                
+                    
                 </div>
                 
             </div>
