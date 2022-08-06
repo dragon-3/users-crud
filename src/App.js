eec517ea0e9afe150155594d828783869628049c
Sean Narron <seannarron9@gmail.com>
2022-08-06 15:54:01 +0900

    Added createComponent in App.js.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeComponent from './HomeComponent';
+import CreateComponent from "./crud/CreateComponent";
+
 
 function App() {
   return (
     <div className="App">
       <HomeComponent />
+      <CreateComponent />
     </div>
   );
 }
