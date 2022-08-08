b6187229169f9adf90c703850a24a4c34bdcbf49
Sean Narron <seannarron9@gmail.com>
2022-08-08 12:33:14 +0900

    Mapped data from api.

--- src/HomeComponent.jsx
+++ src/HomeComponent.jsx
@@ -9,8 +9,8 @@ class HomeComponent extends React.Component {
         super(); 
         this.state = {
             count: 0,
-            // url: "https://62eddc42a785760e677041dd.mockapi.io/crud"
-            url: "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e7946f7765931c840a35548ad3fa50c4",
+            url: "https://62eddc42a785760e677041dd.mockapi.io/crud",
+            // url: "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e7946f7765931c840a35548ad3fa50c4",
             data: []
         }
     }
@@ -18,7 +18,7 @@ class HomeComponent extends React.Component {
     getData = () => {
         axios.get(this.state.url).then((response) => {
             this.setState({
-                data: [response.data]
+                data: response.data
             })
         })
     }
@@ -39,12 +39,23 @@ class HomeComponent extends React.Component {
                     <div className="table">
                         <table>
                             <thead>
-                                <th>City</th>
-                                <th>Weather</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                {/* <th>City</th>
+                                <th>Weather</th> */}
                             </thead>
                             <tbody>
-
-                                {this.state.data.map(
+                                {
+                                    this.state.data.map(
+                                        data => (
+                                            <tr>
+                                                <td>{data.firstName}</td>
+                                                <td>{data.lastName}</td>
+                                            </tr>
+                                        )
+                                    )
+                                }
+                                {/* {this.state.data.map(
                                     data => (
                                             <tr key={data.id}>
                                                 <td>{data.name}</td>
@@ -61,7 +72,7 @@ class HomeComponent extends React.Component {
                                             
                                         )
                                     )
-                                }
+                                } */}
                                 
                             </tbody>
                         </table>
