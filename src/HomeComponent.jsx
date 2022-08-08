e78f49201f9412e2e0e39d1dbd7e3f51c9f81e79
Sean Narron <seannarron9@gmail.com>
2022-08-08 18:11:26 +0900

    Modified.

--- src/HomeComponent.jsx
+++ src/HomeComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import axios from "axios";
+import CrudComponent from "./crud/CrudComponent";
 
 
 class HomeComponent extends React.Component {
@@ -10,6 +11,7 @@ class HomeComponent extends React.Component {
         this.state = {
             count: 0,
             url: "https://62eddc42a785760e677041dd.mockapi.io/crud",
+            // url: `https://f201a4cb-6177-4458-a75c-412cdc1e5069.mock.pstmn.io/crud`,
             // url: "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e7946f7765931c840a35548ad3fa50c4",
             data: []
         }
@@ -18,7 +20,7 @@ class HomeComponent extends React.Component {
     getData = () => {
         axios.get(this.state.url).then((response) => {
             this.setState({
-                data: response.data
+                data: [response.data]
             })
         })
     }
@@ -26,6 +28,13 @@ class HomeComponent extends React.Component {
     componentDidMount = () => {
         this.getData();
     }
+
+    // deleteUser = (id) => {
+    //     axios.delete(`https://62eddc42a785760e677041dd.mockapi.io/crud/${id}`)
+    //     .then(() => {
+    //         this.getData();
+    //     })
+    // }
 
     render() {
         return(
@@ -39,22 +48,34 @@ class HomeComponent extends React.Component {
                     <div className="table">
                         <table>
                             <thead>
-                                <th>First Name</th>
-                                <th>Last Name</th>
+                                <th>City</th>
+                                <th>Weather</th>
+                                <th>Actions</th>
                                 {/* <th>City</th>
                                 <th>Weather</th> */}
                             </thead>
                             <tbody>
+
+        
+                                
+
+                                {/* mock api data */}
                                 {
                                     this.state.data.map(
                                         data => (
-                                            <tr>
+                                            <tr key={data.id}>
                                                 <td>{data.firstName}</td>
                                                 <td>{data.lastName}</td>
+                                                <td>
+                                                    <button className="update-btn" >Update</button>
+                                                    <button className="delete-btn">DELETE</button>
+                                                </td>
                                             </tr>
+                                            
                                         )
                                     )
                                 }
+
                                 {/* {this.state.data.map(
                                     data => (
                                             <tr key={data.id}>
@@ -63,11 +84,17 @@ class HomeComponent extends React.Component {
                                                         data.weather.map(
                                                             weather => (
                                                                 <td>{weather.description}</td> 
+                                                                
 
                                                             )
                                                         )
                                                         
                                                     }
+                                                    <td>
+                                                    <button className="update-btn" >Update</button>
+                                                    <button className="delete-btn" >DELETE</button>
+                                                    
+                                                </td>
                                             </tr>
                                             
                                         )
