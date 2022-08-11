b97e8f21580846219caedb2bb8b8e1dfcf5ddf65
Sean Narron <seannarron9@gmail.com>
2022-08-11 15:52:59 +0900

    Modified.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import HomeComponent from './HomeComponent';
 import CrudComponent from "./crud/CrudComponent";
+import EditDataComponent from './EditDataComponent';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
