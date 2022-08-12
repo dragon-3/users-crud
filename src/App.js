8140cbc6fbef89aa2202412a9c5da1f034a0bd98
Sean Narron <seannarron9@gmail.com>
2022-08-12 16:50:34 +0900

    Modified.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import HomeComponent from './HomeComponent';
 import CrudComponent from "./crud/CrudComponent";
 import EditDataComponent from './EditDataComponent';
+import UpdateComponent from './crud/UpdateComponent';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Switch
 } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <HomeComponent />
-      {/* <CrudComponent /> */}
-    </div>
+      <div className="App">
+      <Router>
+        <>
+          <Switch>
+          
+            <Route path="/:id" exact component={UpdateComponent} /> 
+            <Route path="/" component={HomeComponent} />
+            </Switch>
+        </>
+      </Router> 
+      </div>
+
   );
 }
 
